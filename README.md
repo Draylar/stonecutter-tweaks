2d93e11c3f1a1042142ac8ca548e046a7fc379fe
Draylar <samuelhimes@gmail.com>
2020-06-23 17:22:45 -0500

    update to 1.16

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ![](https://i.imgur.com/Eyqos8E.png)
 
-This is a Fabric mod for 1.15.2 that solves the above issue.
+This is a Fabric mod which solves the above issue.
 
 CC0, do what you want. Let me know if there are any other vital stonecutter tweaks I'm missing. Required on the client and server.
--- src/main/java/draylar/stonecuttertweaks/mixin/StonecutterContainerMixin.java
+++ src/main/java/draylar/stonecuttertweaks/mixin/StonecutterContainerMixin.java
@@ -1,17 +1,17 @@
 package draylar.stonecuttertweaks.mixin;
 
-import net.minecraft.container.Property;
-import net.minecraft.container.StonecutterContainer;
+import net.minecraft.screen.Property;
+import net.minecraft.screen.StonecutterScreenHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(StonecutterContainer.class)
+@Mixin(StonecutterScreenHandler.class)
 public class StonecutterContainerMixin {
 
     @Redirect(
             method = "updateInput",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/container/Property;set(I)V")
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/screen/Property;set(I)V")
     )
     private void beforeReturn(Property property, int value) {
 
